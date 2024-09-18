1d724b4a25cfa5fa300306f3fc749f78d8825880
Kauanny Sibert <127500258+KauannySibert@users.noreply.github.com>
2024-09-18 12:12:45 -0300

    Add files via upload

---
+++ cadastro.js
@@ -0,0 +1,11 @@
+function cadastro_cliente(){
+    const nome_client = document.getElementById ('firstname').value;
+
+
+
+
+
+    document.getElementById('firstname').innerText=nome_client;
+}
+
+alert ('oi')
