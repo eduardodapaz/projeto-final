c046464cf0b544a1c30693b8b2907a02f90cfbbc
Kauanny Sibert <127500258+KauannySibert@users.noreply.github.com>
2024-09-19 09:36:55 -0300

    Add files via upload

--- prjeto final/cadastro.js
+++ prjeto final/cadastro.js
@@ -1,11 +1,23 @@
 function cadastro_cliente(){
     const nome_client = document.getElementById ('firstname').value;
+    const ultimonome_client = document.getElementById ('lastname').value;
+    const emaill = document.getElementById ('email').value;
+    const numero_cel = document.getElementById ('number').value;
+    const senha = document.getElementById ('pasword').value;
+    const verificar_senha = document.getElementById ('confirmPassword').value;
 
 
 
 
 
+
+    document.getElementById('lastname').innerText=ultimonome_client;
+    document.getElementById('email').innerText=emaill;
     document.getElementById('firstname').innerText=nome_client;
+    document.getElementById('number').innerText=numero_cel;
+    document.getElementById('password').innerText=senha;
+    document.getElementById('confirmPasword').innerText=verificar_senha;
+
 }
 
 alert ('oi')
