75fc4ab7e4a2c6c3865d710521be4d7af868e0d0
Kauanny Sibert <127500258+KauannySibert@users.noreply.github.com>
2024-10-01 08:48:28 -0300

    Add files via upload

--- prjeto final/cadastro.js
+++ prjeto final/cadastro.js
@@ -1,44 +1,44 @@
-
-
-function cadastdo_cliente(){
-    const nome_client = document.getElementById ('firstname').value;
-    const ultimonome_client = document.getElementById ('lastname').value;
+/*parte da kakau */
+function cadastro_cliente(){
+    const nome_client = document.getElementById ('nome usuario').value;
+    const ultimonome_client = document.getElementById ('nome usuario2').value;
     const emaill = document.getElementById ('email').value;
-    const numero_cel = document.getElementById ('number').value;
-    const senha = document.getElementById ('pasword').value;
-    const verificar_senha = document.getElementById ('confirmPassword').value;
+    const numero_cel = document.getElementById ('numero_tel').value;
+    const senha = document.getElementById ('senha').value;
+    const verificar_senha = document.getElementById ('confirm_senha').value;
+    const cpf_user = document.getElementById ('cpf_usuario').value;
+
+    alert ('eba')
 
 
 
-
-
-
-    document.getElementById('lastname').innerText=ultimonome_client;
+    document.getElementById('nome usuario2').innerText=ultimonome_client;
     document.getElementById('email').innerText=emaill;
-    document.getElementById('firstname').innerText=nome_client;
-    document.getElementById('number').innerText=numero_cel;
-    document.getElementById('password').innerText=senha;
-    document.getElementById('confirmPasword').innerText=verificar_senha;
+    document.getElementById('nome usuario').innerText=nome_client;
+    document.getElementById('numero_tel').innerText=numero_cel;
+    document.getElementById('senha').innerText=senha;
+    document.getElementById('confirm_senha').innerText=verificar_senha;
+    document.getElementById('cpf_usuario').innerText=cpf_user;
+
 
 }
+/*parte da kakau fim */
 
 
 function postar_produto(){
-    alert('eeeeeeeee');
+    alert('eeeeeeeee')
 
-    const novo_produto = document.getElementById('nomeProduto').value;
-    const preco_produto = document.getElementById('precoProduto').value;
-    const dec_produto = document.getElementById('descricao').value;
+    const novo_produto = document.getElementById ('nomeProduto').value;
+    const preco_produto = document.getElementById ('precoProduto').value;
+    const dec_produto = document.getElementById ('descricao').value;
 
     let tabela = `
-    <tr>
-    <td> ${novo_produto} </td>
-    <td> ${preco_produto} </td>
-    <td> ${dec_produto} </td> 
-    </tr>`;
+    <tr> ${novo_produto} </tr>;
+    <tr> ${preco_produto} </tr>;
+    <tr> ${dec_produto} </tr>; `;
 
 
-    document.querySelector('#tabela tbody').innerHTML = tabela;
+    document.getElementById('').innerHTML=tabela
     
         
         
