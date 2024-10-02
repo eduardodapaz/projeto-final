23ff3ad3c23d1527487cab25b3aa1878d59acd1a
Kauanny Sibert <127500258+KauannySibert@users.noreply.github.com>
2024-10-02 12:11:33 -0300

    Add files via upload

--- projeto-final-main/prjeto final/cadastro.js
+++ projeto-final-main/prjeto final/cadastro.js
@@ -1,25 +1,24 @@
 /*parte da kakau */
 function cadastro_cliente(){
-    const emaill = document.getElementById ('Email').value;
-    const cpf_user = document.getElementById ('CPF').value;
-    const numero_cel = document.getElementById ('TEL').value;
-    const nome_client = document.getElementById ('NOME1').value;
-    const ultimonome_client = document.getElementById ('NOME2').value;
-    const senha = document.getElementById ('CONFIRMSENHA').value;
-    const verificar_senha = document.getElementById ('CONFIRMSENHA2').value;
+    const emaill = document.getElementById('Email').value;
+    const cpf_user = document.getElementById('CPF').value;
+    const numero_cel = document.getElementById('TEL').value;
+    const nome_client = document.getElementById('NOME1').value;
+    const ultimonome_client = document.getElementById('NOME2').value;
+    const senha = document.getElementById('CONFIRMSENHA').value;
+    const verificar_senha = document.getElementById('CONFIRMSENHA2').value;
 
 
-    document.getElementById('email').innerText=emaill;
-    document.getElementById('CPF').innerText=cpf_user;
-    document.getElementById('TEL').innerText=numero_cel;
-    document.getElementById('NOME1').innerText=nome_client;
-    document.getElementById('NOME2').innerText=ultimonome_client;
-    document.getElementById('CONFIRMSENHA').innerText=senha;
-    document.getElementById('CONFIRMSENHA2').innerText=verificar_senha;
-
-
-    alert("scrr")
+    document.getElementById('Email1').innerHTML=emaill;
+    document.getElementById('CPF1').innerHTML=cpf_user;
+    document.getElementById('TEL1').innerHTML=numero_cel;
+    document.getElementById('NOME11').innerHTML=nome_client;
+    document.getElementById('NOME21').innerHTML=ultimonome_client;
+    document.getElementById('CONFIRMSENHA1').innerHTML=senha;
+    document.getElementById('CONFIRMSENHA21').innerHTML=verificar_senha;
 }
+
+alert('sld')
 /*parte da kakau fim */
 
 function postar_produto(){
